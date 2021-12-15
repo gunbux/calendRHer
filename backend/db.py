7036def0eccaeebf421ed29402526608e8b1350c
chun <lamchunyu00@gmail.com>
2021-12-15 14:16:38 +0800

    Updated db

--- backend/db.py
+++ backend/db.py
@@ -6,4 +6,4 @@ DB_PWD = "ilov3RH"
 URL = "mongodb+srv://{}:{}@rhdevs-be-mongo.dv2ho.mongodb.net/admin".format(DB_USERNAME,DB_PWD)
 
 client = pymongo.MongoClient(URL)
-db = client["calendRHer"]
+db = client["calendher"]
