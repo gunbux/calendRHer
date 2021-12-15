be3a595fe2c4fb4339342e07680f647f95f1b9c7
chun <lamchunyu00@gmail.com>
2021-12-15 14:05:46 +0800

    Implemented Redux Form

---
+++ backend/db.py
@@ -0,0 +1,9 @@
+import pymongo
+
+DB_USERNAME = "chunyu"
+DB_PWD = "ilov3RH"
+
+URL = "mongodb+srv://{}:{}@rhdevs-be-mongo.dv2ho.mongodb.net/admin".format(DB_USERNAME,DB_PWD)
+
+client = pymongo.MongoClient(URL)
+db = client["calendRHer"]
